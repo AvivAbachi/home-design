2e526d4af60b849b31b8d72a05d75c3ddcbcd012
AvivAbachi <AvivAbachi@Gmail.com>
2020-07-13 17:22:09 +0300

    Updates

---
+++ precache-manifest.b67fbfeb7d40302236f74eb1eaa23785.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff52e3c0e1a5403ddc5279bf61a7517f",
+    "url": "/home-design/index.html"
+  },
+  {
+    "revision": "71122971b3147036fb41",
+    "url": "/home-design/static/css/4.ec20de7a.chunk.css"
+  },
+  {
+    "revision": "b9071428c2d20d5e84fa",
+    "url": "/home-design/static/css/5.b85432c9.chunk.css"
+  },
+  {
+    "revision": "0830a2b98896500aa8bb",
+    "url": "/home-design/static/css/6.c8c29d31.chunk.css"
+  },
+  {
+    "revision": "2c5ca79d0dc999b8f62c",
+    "url": "/home-design/static/css/main.bc463a1f.chunk.css"
+  },
+  {
+    "revision": "38caf491fc57188f03a5",
+    "url": "/home-design/static/js/0.499a596f.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/home-design/static/js/0.499a596f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1464631a0e22fd373ca6",
+    "url": "/home-design/static/js/3.f0ca66dc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/home-design/static/js/3.f0ca66dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71122971b3147036fb41",
+    "url": "/home-design/static/js/4.30a5ebb1.chunk.js"
+  },
+  {
+    "revision": "b9071428c2d20d5e84fa",
+    "url": "/home-design/static/js/5.a495a8e6.chunk.js"
+  },
+  {
+    "revision": "0830a2b98896500aa8bb",
+    "url": "/home-design/static/js/6.7323b43b.chunk.js"
+  },
+  {
+    "revision": "2c5ca79d0dc999b8f62c",
+    "url": "/home-design/static/js/main.313d270d.chunk.js"
+  },
+  {
+    "revision": "b062dbd9121fb4cc06e2",
+    "url": "/home-design/static/js/runtime-main.0dc166c1.js"
+  },
+  {
+    "revision": "d725c68d48513095a5f14fdf64cccac9",
+    "url": "/home-design/static/media/0afcaf58-2c3f-4fb7-aee8-06ac4486fe52.d725c68d.webp"
+  },
+  {
+    "revision": "74840125f4ee0d72b9a5f0e0420fd5f4",
+    "url": "/home-design/static/media/0f7e7aad-e1dd-4ca5-b86a-7d75ac28831f.74840125.webp"
+  },
+  {
+    "revision": "a4ed84b0eb3fc8ec8517f68705112b80",
+    "url": "/home-design/static/media/10349766.a4ed84b0.webp"
+  },
+  {
+    "revision": "e8d54b4c17b1f5de759a43ac040e7dc8",
+    "url": "/home-design/static/media/12795411.e8d54b4c.webp"
+  },
+  {
+    "revision": "6759d5c763bab0e4d30ae5b2a1733cbd",
+    "url": "/home-design/static/media/22871a48-5aa6-4f83-8e98-dd2efb6823c6.6759d5c7.webp"
+  },
+  {
+    "revision": "b7ea3c92683a489f60ad8e33396d45f9",
+    "url": "/home-design/static/media/23547755.b7ea3c92.webp"
+  },
+  {
+    "revision": "e0c32b1f99c3577fc1dd36818f5e3d56",
+    "url": "/home-design/static/media/30e47c06-7f57-41da-ae80-42d0b6c67f34.e0c32b1f.webp"
+  },
+  {
+    "revision": "6dfdea5f9cf7eb2f80c515fe17b29ea5",
+    "url": "/home-design/static/media/34679add-f337-4ef7-aacc-7d22b582e6a1.6dfdea5f.webp"
+  },
+  {
+    "revision": "c0d5a954bd903223e825995be9c0ef90",
+    "url": "/home-design/static/media/47abb7e8-7ca2-4634-8b1f-e42525a2182e.c0d5a954.webp"
+  },
+  {
+    "revision": "ba89da201b50a16f3b80b612c876275c",
+    "url": "/home-design/static/media/566ee31c-f92a-4c15-9d0f-b806191506a4.ba89da20.webp"
+  },
+  {
+    "revision": "274f1c3c87df76075c6c4b50c20c6838",
+    "url": "/home-design/static/media/64820336.274f1c3c.webp"
+  },
+  {
+    "revision": "61dd7bf97aa54cfc11e7b0f23d42c510",
+    "url": "/home-design/static/media/749ced26-ccf0-47f8-b24f-ba6332156f22.61dd7bf9.webp"
+  },
+  {
+    "revision": "7c3ff28bc3499faa9761d33e867e5717",
+    "url": "/home-design/static/media/77174273.7c3ff28b.webp"
+  },
+  {
+    "revision": "ce956685ff204bfabda43dd42e4f1dc9",
+    "url": "/home-design/static/media/88895746.ce956685.webp"
+  },
+  {
+    "revision": "1f1e08e7133be865f97e6f891666d107",
+    "url": "/home-design/static/media/9391ffa3-2cb4-4a66-b4eb-eb2f3284dfa6.1f1e08e7.webp"
+  },
+  {
+    "revision": "c4ea5e2549bb4a5f2cbe202cbb31d514",
+    "url": "/home-design/static/media/Armoires & Wardrobes.c4ea5e25.webp"
+  },
+  {
+    "revision": "06e0b705d4f56a8939b02084d80e66ef",
+    "url": "/home-design/static/media/Beds.06e0b705.webp"
+  },
+  {
+    "revision": "671fe18cb27c35be50e55ac3054fc082",
+    "url": "/home-design/static/media/Bookcases.671fe18c.webp"
+  },
+  {
+    "revision": "522765a60796327f9ffd8a3218178631",
+    "url": "/home-design/static/media/Chairs.522765a6.webp"
+  },
+  {
+    "revision": "631f115f721860f18263035f99fd38ca",
+    "url": "/home-design/static/media/Coffee Tables.631f115f.webp"
+  },
+  {
+    "revision": "4288700ea930aebaf146d2575f5a6a44",
+    "url": "/home-design/static/media/Dining Room Sets.4288700e.webp"
+  },
+  {
+    "revision": "d87c2e81020f06fd6a92a01cc6a3cd54",
+    "url": "/home-design/static/media/Dressers.d87c2e81.webp"
+  },
+  {
+    "revision": "3be172eb5c715c6e87287012e5445faa",
+    "url": "/home-design/static/media/Kitchen & Appliances.3be172eb.webp"
+  },
+  {
+    "revision": "faa7656f28b13fa334d411a755c64f31",
+    "url": "/home-design/static/media/Logo.faa7656f.svg"
+  },
+  {
+    "revision": "5eebc13770a4bbb8e200782b05072212",
+    "url": "/home-design/static/media/Sofas & Couches.5eebc137.webp"
+  },
+  {
+    "revision": "c37656776940b74a0eee4bdc31e39b41",
+    "url": "/home-design/static/media/TV Stands.c3765677.webp"
+  },
+  {
+    "revision": "4f087f42a2ec5d8506e6e98352fe0f01",
+    "url": "/home-design/static/media/chevron-left.4f087f42.svg"
+  },
+  {
+    "revision": "57502ca56a06a00919baddb60f5e87a7",
+    "url": "/home-design/static/media/chevron-right.57502ca5.svg"
+  },
+  {
+    "revision": "5fdcb9b67df9a30e400e9a12c486bcd4",
+    "url": "/home-design/static/media/dd59306c-f961-4578-8746-75d18dc985ac.5fdcb9b6.webp"
+  },
+  {
+    "revision": "0888d2fff78cafa149acf1a2c324efd3",
+    "url": "/home-design/static/media/neutra-text-bold-5871e1605afa2.0888d2ff.woff"
+  },
+  {
+    "revision": "648bc2e355694a0740d588ad0b58dcf9",
+    "url": "/home-design/static/media/neutra-text-bold-italic-5872615974382.648bc2e3.woff"
+  },
+  {
+    "revision": "359ddf8803056e18c5d8f83d031def9b",
+    "url": "/home-design/static/media/neutra-text-light-5872613aeda5b.359ddf88.woff"
+  },
+  {
+    "revision": "3940087712316266524b1dcd15545b03",
+    "url": "/home-design/static/media/neutra-text-tf-5871e0b0704ed.39400877.woff"
+  },
+  {
+    "revision": "162e0a988bb24dd1787aaeae73073037",
+    "url": "/home-design/static/media/slide1.162e0a98.webp"
+  },
+  {
+    "revision": "fe3cf461d62d584db8736bac3eaff368",
+    "url": "/home-design/static/media/specialPrice1.fe3cf461.webp"
+  },
+  {
+    "revision": "e6fed146f4bd83cf5824428a1b277ab6",
+    "url": "/home-design/static/media/specialPrice2.e6fed146.webp"
+  },
+  {
+    "revision": "cd797ea77814771d87d38ed0ac4efa33",
+    "url": "/home-design/static/media/specialPrice3.cd797ea7.webp"
+  },
+  {
+    "revision": "5f18e0d8a17d4dad0929de8c3f64c341",
+    "url": "/home-design/static/media/specialPrice4.5f18e0d8.webp"
+  },
+  {
+    "revision": "228dfb28e1b43747d9802781d7770fd7",
+    "url": "/home-design/static/media/strip.228dfb28.webp"
+  },
+  {
+    "revision": "825335846c65ffe98dff2fab4d81cee6",
+    "url": "/home-design/static/media/strip.82533584.webp"
+  }
+]);
